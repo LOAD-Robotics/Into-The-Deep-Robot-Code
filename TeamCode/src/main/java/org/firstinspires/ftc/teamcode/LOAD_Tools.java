83418e94602699f0db9ce17baf487d6ba625589c
Dan <141444315+Professor348@users.noreply.github.com>
2025-03-18 09:31:19 -0400

    Renamed mecanumDriving to robotCentricDriving, and wrote fieldCentricDriving function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LOAD_Tools.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LOAD_Tools.java
@@ -37,11 +37,55 @@ public class LOAD_Tools {
      * @param inpArray The array formatted as {leftStickX, leftStickY, rightStickX}
      * */
 
-    public float[] mecanumDriving(float[] inpArray) {
+    public float[] robotCentricDriving(float[] inpArray) {
         // Separate out the values for each of the joystick positions
         float X = inpArray[0];
         float Y = inpArray[1];
         float rX = inpArray[2];
+
+        // Do some calculations in order to obtain a number that is used to ensure
+        // that the outputs of this function are constrained to the interval [-1,1]
+        float d = (float) JavaUtil.maxOfList(JavaUtil.createListWith((Math.abs(Y) + Math.abs(X) + Math.abs(rX)), 1));
+
+        // Calculate the values for each of the drive motors
+        float FL = (((Y + X + rX) / d));
+        float BL = (((Y - X + rX) / d));
+        float FR = (((Y - X - rX) / d));
+        float BR = (((Y + X - rX) / d));
+
+        // Define and assign values to the output variable
+        float[] output = new float[4];
+        output[0] = FL;
+        output[1] = BL;
+        output[2] = FR;
+        output[3] = BR;
+
+        // Return the calculated motor powers
+        return output;
+    }
+
+    /**
+     * Takes inputs of the joystick positions and robot heading,
+     * and returns 4 values for each of the 4 motor powers
+     *
+     * @param inpArray The array formatted as {leftStickX, leftStickY, rightStickX, robotHeading}
+     * */
+
+    public float[] fieldCentricDriving(float[] inpArray) {
+        // Separate out the values for each of the joystick positions
+        float X = inpArray[0];
+        float Y = inpArray[1];
+        float rX = inpArray[2];
+        float h = inpArray[3];
+
+        // Perform a vector rotation of the left stick based on the robot's heading
+        float[] stickVector = new float[3];
+        stickVector[0] = X;
+        stickVector[1] = Y;
+        stickVector[2] = -h;
+        float[] correctedVector = rotateVector(stickVector);
+        X = correctedVector[0];
+        Y = correctedVector[1];
 
         // Do some calculations in order to obtain a number that is used to ensure
         // that the outputs of this function are constrained to the interval [-1,1]
